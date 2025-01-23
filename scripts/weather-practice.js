c45c4714662317939df60b4f8308f8321adc9798
lifegatejustice <lifegatedetom@gmail.com>
2025-01-23 09:24:28 +0100

    `Added weather API functionality to weather-practice.html, scripts/weather-practice.js, and updated chamber/styles/directory.css`

--- scripts/weather-practice.js
+++ scripts/weather-practice.js
@@ -1,18 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
-    const cards = document.querySelector('#cards');
-    let allProphets = []; // Store all prophets data
-  
-    async function getProphetData() {
-      try {
-        const response = await fetch(url);
+  const currentTemp = document.querySelector('#current-temp');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('figcaption');
+
+  const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.749992&lon=6.637143&units=metric&appid=cb9dc7fc689e41f4fe2dd4c5d716b04a';
+
+  async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
         const data = await response.json();
-        console.table(data.prophets); // Log the fetched data for debugging
-        allProphets = data.prophets; // Store the data for filtering
-        displayProphets(allProphets); // Send the prophets array to displayProphets
-      } catch (error) {
-        console.error('Error fetching prophet data:', error);
+        console.log(data); // testing only
+        displayResults(data); // uncomment when ready
+      } else {
+        throw Error(await response.text());
       }
+    } catch (error) {
+      console.log(error);
     }
-  
-    getProphetData();
+  }
+
+  apiFetch();
+
+  function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
+})
+
+ 
